06de4d8270b23d83ac5d41f87aee4cffd15de5ac
Invisible <kp@yeahunter.hu>
2013-04-28 16:05:38 +0200

    most mar csinal screenshotot Signed-off-by: Invisible <kp@yeahunter.hu>

--- GrabTheMoment/Form1.cs
+++ GrabTheMoment/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows;
+using System.Drawing.Imaging;
 
 namespace GrabTheMoment
 {
@@ -15,6 +17,17 @@ namespace GrabTheMoment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int screenheight = Screen.PrimaryScreen.Bounds.Height;
+            int screenwidth = Screen.PrimaryScreen.Bounds.Width;
+            Bitmap bmpScreenShot = new Bitmap(screenwidth, screenheight);
+            Graphics gfx = Graphics.FromImage((Image)bmpScreenShot);
+            gfx.CopyFromScreen(0, 0, 0, 0, new Size(screenwidth, screenheight));
+            bmpScreenShot.Save(DateTime.Now.ToString("yyyy.MM.dd.-HH.mm.ss") + ".png", ImageFormat.Png);
+            //this.Text = Screen.PrimaryScreen.Bounds.Height.ToString();
         }
     }
 }
