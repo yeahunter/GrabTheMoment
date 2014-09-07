964b1dc3c9a5d35ab62598fb14c66814251649a5
Invisible <yeahunter@gmail.com>
2014-09-08 00:00:46 +0200

    elirtam a szamitast eca542be506df296306a86dff9bf3782573eef54
    Signed-off-by: Invisible <yeahunter@gmail.com>

--- GrabTheMoment/ScreenMode/ActiveWindow.cs
+++ GrabTheMoment/ScreenMode/ActiveWindow.cs
@@ -9,11 +9,11 @@ namespace GrabTheMoment.ScreenMode
         public ActiveWindow(Rectangle rectangle)
         {
             SetFileName();
-            Height = rectangle.Height - Y;
-            Width = rectangle.Width - X;
-
             X = rectangle.X;
             Y = rectangle.Y;
+
+            Height = rectangle.Height - Y;
+            Width = rectangle.Width - X;
 
             if (X == -8 && Y == -8)
             {
